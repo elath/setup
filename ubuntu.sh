1a594453c886f8a37280b7c1613ae2eb15a3ed91
Bryce Klippenstein <bryceklippenstein@gmail.com>
2024-10-13 18:07:12 +0000

    Interm commit

---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo su
+install -o root -g root -m 440 ./sudoers /etc/sudoers
+
+# git config --global user.name "Bryce Klippenstein"
+# git config --global user.email "bryceklippenstein@gmail.com"
