5753e08c160c392cfdba8be7aa1b72518a0b920a
Bryce Klippenstein <bryceklippenstein@gmail.com>
2024-10-13 19:47:42 +0000

    Release 1.0

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-sudo su
-install -o root -g root -m 440 ./sudoers /etc/sudoers
+user=$(whoami)
+echo $user
 
-# git config --global user.name "Bryce Klippenstein"
-# git config --global user.email "bryceklippenstein@gmail.com"
+wget -O sudoers.file https://raw.githubusercontent.com/elath/setup/refs/heads/main/sudoers.file
+sudo visudo sh -c -f ./sudoers.file
+
+sudo apt update
+sudo apt dist-upgrade
+sudo apt install -y vim curl zsh git qemu-guest-agent
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i -e "s|robbyrussell|alanpeabody|g" /home/$user/.zshrc
+sudo cp -r /home/$user/.oh-my-zsh /root
+sudo cp /home/$user/.zshrc /root
+
+sudo chsh -s /usr/bin/zsh $user
+sudo chsh -s /usr/bin/zsh root
+
+git config --global user.name "Bryce Klippenstein"
+git config --global user.email "bryceklippenstein@gmail.com"
